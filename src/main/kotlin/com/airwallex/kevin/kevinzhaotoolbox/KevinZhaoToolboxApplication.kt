3ef02f7a86d4cf19d2129cc208bafe106ab62325
Kevin.Zhao <kevin.zhao@airwallex.com>
2021-09-28 10:35:42 +0800

    init

---
+++ src/main/kotlin/com/airwallex/kevin/kevinzhaotoolbox/KevinZhaoToolboxApplication.kt
@@ -0,0 +1,11 @@
+package com.airwallex.kevin.kevinzhaotoolbox
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KevinZhaoToolboxApplication
+
+fun main(args: Array<String>) {
+	runApplication<KevinZhaoToolboxApplication>(*args)
+}
---
+++ src/test/kotlin/com/airwallex/kevin/kevinzhaotoolbox/KevinZhaoToolboxApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.airwallex.kevin.kevinzhaotoolbox
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KevinZhaoToolboxApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
